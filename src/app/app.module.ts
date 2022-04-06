aaa58ddde3e9b00ca01f50384a3aaa92dcfca771
Rodrigo <48496310+RodrigoDisconsi@users.noreply.github.com>
2022-04-06 19:44:05 -0300

    Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { HomeComponent } from './componentes/home/home.component';
 
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,9 +29,13 @@ import { HomeComponent } from './componentes/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     NgbModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/login-register/login/login.component.ts
+++ src/app/componentes/login-register/login/login.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['../login-register.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public cargando: boolean = false;
+  public hide: boolean = false;
+  public usuario: string = "";
+  public error: boolean = false;
+  public errorMsj:string = "";
+  public loginForm = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  ngOnInit(): void {
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder,
+    private auth: AuthService) {
+
+  }
+
+  ngOnInit() { }
+
+  onLogin() {
+    this.cargando = true;
+    this.error = false;
+    this.auth.login(this.loginForm.value.username, this.loginForm.value.password).then((user) => {
+      if (user) {
+        // this.router.navigateByUrl("");
+        this.auth.user = user.user;
+        this.auth.loggedIn.next(true);
+        localStorage.setItem('nickname', this.auth.user.displayName);
+        this.router.navigate(['']);
+        this.cargando = false;
+      }
+    }).catch(e => {
+      this.errorMsj = e.message;
+      this.error = true;
+      this.cargando = false;
+      console.info("ERROR ->", e);
+    });
+  }
+
+  getErrorMessage(field: string): string {
+    let retorno = "";
+    if(this.loginForm.get(field)?.hasError("required")) {
+      retorno = "Campo vacío.";
+    }
+    else if(this.loginForm.get(field)?.hasError("minlength")) {
+      retorno = "Mínimo de carácteres 6";
+    }
+    else if(this.loginForm.get(field)?.hasError("email")){
+      retorno = "Not valid email.";
+    }
+    return retorno;
+  }
+
+  isNotValidField(field: string): boolean {
+    return (this.loginForm.get(field)?.touched || this.loginForm.get(field)?.dirty == true)
+      && !this.loginForm.get(field)?.valid;
+  }
+
+  completarLogin() {
+    this.loginForm.setValue({ username: "usuario@gmail.com", password: "123456" });
   }
 
 }
