eac3c1205f963dd85204e480809fc9c28c285a5a
rodrigodisconsi@gmail.com <RodrigoDisconsi@gmail.com>
2022-04-07 19:09:09 -0300

    fix heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDh5br--eUK4STYRGPHjLaQeZhxApIamjI",
+    authDomain: "tp-sala-de-juegos-92c8a.firebaseapp.com",
+    databaseURL: "https://tp-sala-de-juegos-92c8a.firebaseio.com",
+    projectId: "tp-sala-de-juegos-92c8a",
+    storageBucket: "tp-sala-de-juegos-92c8a.appspot.com",
+    messagingSenderId: "438502520653",
+    appId: "1:438502520653:web:c474ed7aba055c54e22904",
+    measurementId: "G-WX23KT33S7"
+  }
 };
