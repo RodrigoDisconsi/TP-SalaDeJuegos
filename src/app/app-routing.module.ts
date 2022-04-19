b6c9c075c8251ca1dcf20433af619c9339e9c97f
rodrigodisconsi@gmail.com <RodrigoDisconsi@gmail.com>
2022-04-19 19:16:03 -0300

    fix heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginRegisterComponent } from './componentes/login-register/login-register.component';
+import { LoginActivateGuard } from './guard/login-activate.guard';
 
 const routes: Routes = [
-  {path: '' , component: HomeComponent},
+  {path: '' , component: HomeComponent, canActivate:[LoginActivateGuard]},
   {path: 'Login' , component: LoginRegisterComponent}
 ];
 
